9fba02093681bace23d1b1681dd2a9ec1ba0cf55
Pavel Rusin <pa-rusin@yandex.ru>
2020-06-25 00:10:33 +0300

    finished_project

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const editButton = document.querySelector('.profile__button_edit');
+const popup = document.querySelector('.popup');
+const fullname = document.querySelector('.profile__fullname');
+const subtitle = document.querySelector('.profile__subtitle');
+const submit = document.querySelector('.popup__submit');
+const fullnameInput = document.querySelector('.popup__fullname');
+const subtitleInput = document.querySelector('.popup__subtitle');
+const exitButton = document.querySelector('.popup__close');
+
+editButton.addEventListener('click', popupDisplayOn);
+exitButton.addEventListener('click', popupDisplayOff);
+
+submit.addEventListener('click', saveChange);
+
+function popupDisplayOn() {
+    popup.style.display = 'flex'
+    fullnameInput.value = fullname.innerText
+    subtitleInput.value = subtitle.innerText
+}
+
+
+function popupDisplayOff() {
+    popup.style.display = 'none'
+}
+
+function saveChange() {
+    fullname.innerText = fullnameInput.value
+    subtitle.innerText = subtitleInput.value
+    popupDisplayOff()
+}
